644c187a2c1c0890fd874ecdaf7b4cbc80301ef7
Finn Catling <finn.catling@gmail.com>
2020-05-28 08:31:33 +0100

    test

--- utils/model/albumin.py
+++ utils/model/albumin.py
@@ -15,31 +15,31 @@ def albumin_model_factory(
     return GammaGAM(
         s(columns.get_loc('S01AgeOnArrival'), lam=500) +
         # s(columns.get_loc('S03SerumCreatinine'), lam=500) +
-        s(columns.get_loc('S03Sodium'), lam=400)
-        # s(columns.get_loc('S03Potassium'), lam=300) +
-        # s(columns.get_loc('S03Urea'), lam=400) +
-        # s(columns.get_loc('S03WhiteCellCount'), lam=400) +
-        # s(columns.get_loc('S03SystolicBloodPressure'), lam=400) +
-        # s(columns.get_loc('S03GlasgowComaScore'), lam=150, n_splines=13) +
-        # f(columns.get_loc('S03ASAScore'), coding='dummy') +
-        # te(columns.get_loc('S03Pred_Peritsoil'),
-        #    columns.get_loc('S02PreOpCTPerformed'),
-        #    lam=(2, 1),
-        #    n_splines=(len(multi_cat_levels['S03Pred_Peritsoil']), 2),
-        #    spline_order=(0, 0),
-        #    dtype=('categorical', 'categorical')) +
-        # te(columns.get_loc(indication_var_name),
-        #    columns.get_loc('S02PreOpCTPerformed'),
-        #    lam=(2, 1),
-        #    n_splines=(len(multi_cat_levels[indication_var_name]), 2),
-        #    spline_order=(0, 0),
-        #    dtype=('categorical', 'categorical')) +
-        # te(columns.get_loc('S03Pulse'),
-        #    columns.get_loc('S03ECG'),
-        #    lam=(400, 2),
-        #    n_splines=(20, 2),
-        #    spline_order=(3, 0),
-        #    dtype=('numerical', 'categorical'))
+        s(columns.get_loc('S03Sodium'), lam=400) +
+        s(columns.get_loc('S03Potassium'), lam=300) +
+        s(columns.get_loc('S03Urea'), lam=400) +
+        s(columns.get_loc('S03WhiteCellCount'), lam=400) +
+        s(columns.get_loc('S03SystolicBloodPressure'), lam=400) +
+        s(columns.get_loc('S03GlasgowComaScore'), lam=150, n_splines=13) +
+        f(columns.get_loc('S03ASAScore'), coding='dummy') +
+        te(columns.get_loc('S03Pred_Peritsoil'),
+           columns.get_loc('S02PreOpCTPerformed'),
+           lam=(2, 1),
+           n_splines=(len(multi_cat_levels['S03Pred_Peritsoil']), 2),
+           spline_order=(0, 0),
+           dtype=('categorical', 'categorical')) +
+        te(columns.get_loc(indication_var_name),
+           columns.get_loc('S02PreOpCTPerformed'),
+           lam=(2, 1),
+           n_splines=(len(multi_cat_levels[indication_var_name]), 2),
+           spline_order=(0, 0),
+           dtype=('categorical', 'categorical')) +
+        te(columns.get_loc('S03Pulse'),
+           columns.get_loc('S03ECG'),
+           lam=(400, 2),
+           n_splines=(20, 2),
+           spline_order=(3, 0),
+           dtype=('numerical', 'categorical'))
     )
 
 
