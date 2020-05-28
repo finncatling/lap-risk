b371f11874f813190089af48e5e96751dc91f9ff
Finn Catling <finn.catling@gmail.com>
2020-05-28 12:15:44 +0100

    debug

--- utils/plot/pdp.py
+++ utils/plot/pdp.py
@@ -49,6 +49,7 @@ def plot_partial_dependence(
         row_for_this_term = pdp_term.gs_pos
         if isinstance(row_for_this_term, slice):
             row_for_this_term = row_for_this_term.stop - 1
+        print(row_for_this_term)
         if n_rows < row_for_this_term + 1:
             n_rows = row_for_this_term + 1
     n_rows = int(np.ceil((len(terms)) / n_cols))
