88517d5a260879d6a7bde8ca39115de62c3f36ce
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2020-07-19 21:52:17 +0100

    starting to test imputing

---
+++ tests/test_imputation.py
@@ -0,0 +1,31 @@
+import pytest
+
+from .datafixture import dummy_dataframe
+from utils import impute
+from utils.split import Splitter, TrainTestSplitter
+from utils.model.novel import WINSOR_QUANTILES, NOVEL_MODEL_VARS
+df = dummy_dataframe()
+
+def test_determine_imputations():
+    n_imps, fraction = impute.determine_n_imputations(df)
+    
+    assert n_imps == 59
+
+def test_SWM_1():
+    swm = impute.SplitterWinsorMICE(
+    df=df,
+    train_test_splitter=TrainTestSplitter,
+    target_variable_name=NOVEL_MODEL_VARS["target"],
+    cont_variables=NOVEL_MODEL_VARS["cont"],
+    binary_variables=NOVEL_MODEL_VARS["cat"],
+    winsor_quantiles=(0.2, 0.8),
+    winsor_include={
+        "S01AgeOnArrival": (False, True),
+        "S03GlasgowComaScore": (False, False),
+    },
+    n_mice_imputations=5,
+    n_mice_burn_in=10,
+    n_mice_skip=3,
+    )
+
+    swm.split_winsorize_mice()
--- tests/test_winsorize.py
+++ tests/test_winsorize.py
@@ -7,12 +7,17 @@ df = dummy_dataframe()
 
 def test_winsorise_1():
     winsorised, thresholds = novel.winsorize_novel(df, None, novel.NOVEL_MODEL_VARS["cont"], (0.2, 0.8))
+    
+    
+    #check the columns have changed
     with pytest.raises(AssertionError):
         for i in novel.NOVEL_MODEL_VARS["cont"]:
             np.testing.assert_array_equal(df[i].values, winsorised[i].values)
 
 def test_winsorise_2():
     winsorised, thresholds = novel.winsorize_novel(df, None, novel.NOVEL_MODEL_VARS["cont"], (0.2, 0.8))
+    
+    
     with pytest.raises(AssertionError):
         for i in novel.NOVEL_MODEL_VARS["cont"]:
             np.testing.assert_array_equal(df[i].values, winsorised[i].values)
