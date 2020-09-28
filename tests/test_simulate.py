3951209d85b2c2000e8ea3251a0320557625c7a8
Finn Catling <finn.catling@gmail.com>
2020-09-28 19:28:09 +0100

    parameterised the fixture

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -4,15 +4,11 @@ from utils.simulate import TruncatedDistribution
 
 
 class TestTruncatedDistribution:
-    # @pytest.fixture(scope='class', params=[
-    #     (norm(loc=0, scale=1), -10, 0.3),
-    #     (exponpow(b=75, loc=71, scale=50), 18, 110)])
-    # def truncated_distribution(self, rv, lower_bound, upper_bound):
-    #     return TruncatedDistribution(rv, lower_bound, upper_bound)
-
-    @pytest.fixture(scope='class')
-    def truncated_distribution(self):
-        return TruncatedDistribution(exponpow(b=75, loc=71, scale=50), 18, 110)
+    @pytest.fixture(scope='class', params=[
+        (norm(loc=0, scale=1), -10, 0.3),
+        (exponpow(b=75, loc=71, scale=50), 18, 110)])
+    def truncated_distribution(self, request):
+        return TruncatedDistribution(*request.param)
 
     def test_normaliser(self, truncated_distribution):
         assert truncated_distribution.normaliser <= 1
