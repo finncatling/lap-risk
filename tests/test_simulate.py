e61cc0c07c0bb39d0f84af227a46c44993c893b4
Finn Catling <finn.catling@gmail.com>
2020-09-28 19:24:53 +0100

    switched to using a fixture for the object

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -3,13 +3,17 @@ from scipy.stats import norm, exponpow
 from utils.simulate import TruncatedDistribution
 
 
-@pytest.mark.parametrize(
-    'rv, lower_bound, upper_bound',
-    [(norm(loc=0, scale=1), -10, 0.3),
-     (exponpow(b=75, loc=71, scale=50), 18, 110)]
-)
 class TestTruncatedDistribution:
-    def test_sample(self, rv, lower_bound, upper_bound):
-        td = TruncatedDistribution(rv, lower_bound, upper_bound)
-        assert td.normaliser <= 1
-        assert td.normaliser >= 0
+    # @pytest.fixture(scope='class', params=[
+    #     (norm(loc=0, scale=1), -10, 0.3),
+    #     (exponpow(b=75, loc=71, scale=50), 18, 110)])
+    # def truncated_distribution(self, rv, lower_bound, upper_bound):
+    #     return TruncatedDistribution(rv, lower_bound, upper_bound)
+
+    @pytest.fixture(scope='class')
+    def truncated_distribution(self):
+        return TruncatedDistribution(exponpow(b=75, loc=71, scale=50), 18, 110)
+
+    def test_normaliser(self, truncated_distribution):
+        assert truncated_distribution.normaliser <= 1
+        assert truncated_distribution.normaliser >= 0
