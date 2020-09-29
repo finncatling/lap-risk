280ab41d5a929c94dc5aec0a229c85aee8dd5f5a
Finn Catling <finn.catling@gmail.com>
2020-09-29 16:53:11 +0100

    lap-risk-18 refactor

--- tests/test_novel.py
+++ tests/test_novel.py
@@ -12,7 +12,7 @@ def test_combine(initial_df_fixture):
     assert initial_df_fixture["S03ECG"].unique != combined_df["S03ECG"].unique
 
 
-def test_add_missingness(initial_df_fixture):
+def test_add_missingness_indicators(initial_df_fixture):
     cols = initial_df_fixture.shape[1]
     df2 = novel.add_missingness_indicators(
         initial_df_fixture,
