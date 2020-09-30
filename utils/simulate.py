955d1f9f157a82eae56fca3f69c520fb8abdd723
Finn Catling <finn.catling@gmail.com>
2020-09-30 18:28:57 +0100

    lap-risk-23 moved fixture generation logic out of test suite

--- utils/simulate.py
+++ utils/simulate.py
@@ -1,9 +1,23 @@
+import os
 import numpy as np
 import pandas as pd
 from scipy import stats
 from typing import Iterable
 
+from utils.io import load_object
 from utils.model.novel import get_indication_variable_names
+
+
+def get_initial_df_specification(
+    specification_filepath: str = os.path.join(
+        os.pardir, 'config', 'initial_df_univariate_specification.pkl')
+) -> dict:
+    """Specification for the continuous and categorical variables in the NELA
+        data. Contains all the variables names, the categories (and
+        associated probabilities) for each categorical variable, plus parameters
+        for the parametric distribution that most closely fits the univariate
+        empirical distributions of each continuous variable."""
+    return load_object(specification_filepath)
 
 
 class TruncatedDistribution:
