222fd39fba607cf83fcae5881026d31e1159e908
Finn Catling <finn.catling@gmail.com>
2020-10-01 10:25:55 +0100

    lap-risk-23 changed to loading in new input data

--- 02_train_eval_current_model.py
+++ 02_train_eval_current_model.py
@@ -41,7 +41,7 @@ make_directory(CURRENT_MODEL_OUTPUT_DIR)
 
 reporter.report("Loading manually-wrangled NELA data")
 df = pd.read_pickle(
-    os.path.join(DATA_DIR, "df_after_univariate_wrangling.pkl")
+    os.path.join(DATA_DIR, "lap_risk_df_after_univariate_wrangling.pkl")
 ).reset_index(drop=True)
 
 
