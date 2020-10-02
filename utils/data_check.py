6ea7ec365aeaa982d53efc71905282d803add619
Finn Catling <finn.catling@gmail.com>
2020-10-02 10:05:06 +0100

    lap-risk-24 made separate functions for each stage of load_nela_data_and_sanity_check. tests for these separate functions

--- utils/data_check.py
+++ utils/data_check.py
@@ -4,6 +4,7 @@ from warnings import warn
 
 import numpy as np
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 
 from utils.constants import NELA_DATA_FILEPATH, ROOT_DIR
 from utils.inspect import percent_missing
@@ -32,13 +33,10 @@ def load_nela_data_and_sanity_check(
         0_univariate_wrangling.ipynb). Checks that the data looks like we expect
         it to.
 
-    # TODO: Check (with tolerance) prevalence of each category
-    # TODO: Check (with tolerance) distribution of continuous variables
-
     Args:
         data_filepath: Path to .pkl containing NELA data
-        missingness_warning_fraction: In interval [0, 1]. Shows the user for any
-        column that has more than this fraction of missing values
+        missingness_warning_fraction: In interval [0, 1]. Shows warning for any
+            column that has more than this fraction of missing values
 
     Returns:
         NELA data
@@ -46,7 +44,26 @@ def load_nela_data_and_sanity_check(
     df = pd.read_pickle(data_filepath).reset_index(drop=True)
     spec = get_initial_df_specification()
 
-    # Make a flat list of names of all columns we expect in the NELA data
+    check_nela_data_column_names(df, spec)
+    check_nela_data_types(df)
+    check_nela_data_complete_columns(df, spec)
+    check_nela_data_categories(df, spec)
+    warn_if_high_nela_data_missingness(df, missingness_warning_fraction)
+    # TODO: Check (with tolerance) prevalence of each category
+    # TODO: Check (with tolerance) distribution of continuous variables
+
+    return df
+
+
+def check_nela_data_column_names(df: pd.DataFrame, spec: dict) -> None:
+    """Make a flat list of names of all columns we expect in the NELA data.
+        Check that this column name list has the form we expect, then check
+        that the NELA data contains all and only those columns.
+
+    Args:
+        df: NELA data
+        spec: Initial data specification from get_initial_df_specification()
+    """
     all_spec_column_names: List[str] = (
         spec['var_names']['institutions'] +
         spec['var_names']['cat'] +
@@ -54,16 +71,31 @@ def load_nela_data_and_sanity_check(
         spec['var_names']['indications'] +
         [spec['var_names']['target']]  # this is a str. need to put it in a list
     )
-    # Check that this column name list has the form we expect
     assert isinstance(all_spec_column_names, list)
     assert all(isinstance(col_name, str) for col_name in all_spec_column_names)
-    # Check that the NELA data contains all and only those columns
     assert set(df.columns) == set(all_spec_column_names)
 
-    # Check DataFrame contain only floats, ints and missing values
-    assert np.isreal(df).all()
 
-    # Check that there are no missing variables in some columns
+def check_nela_data_types(df: pd.DataFrame) -> None:
+    """Check that NELA data contain only numeric dtypes. This check should pass
+        with floats, ints and missing values.
+
+    Args:
+        df: NELA data
+    """
+    for column_name in df.columns:
+        assert is_numeric_dtype(df[column_name])
+
+
+def check_nela_data_complete_columns(df: pd.DataFrame, spec: dict) -> None:
+    """Check that some specific columns in the NELA data (the target column,
+        institution ID column and binary indication indicator columns)
+        contain no missing values.
+
+    Args:
+        df: NELA data
+        spec: Initial data specification from get_initial_df_specification()
+    """
     complete_columns = [
         spec['var_names']['target'],
         spec['var_names']['institutions'][0]
@@ -71,24 +103,43 @@ def load_nela_data_and_sanity_check(
     for column_name in complete_columns:
         assert percent_missing(df, column_name) == 0.
 
-    # Check categories for each categorical variable
+
+def check_nela_data_categories(df: pd.DataFrame, spec: dict) -> None:
+    """Check that each categorical variable in the NELA contains all and only
+        the categories we expect.
+
+    Args:
+        df: NELA data
+        spec: Initial data specification from get_initial_df_specification()
+    """
     for var_name, expected_probabilities in spec['cat_fits'].items():
         actual_probabilities = df[var_name].value_counts(
             normalize=True,
             dropna=True
         )
-        # Check all and only expected categories are present
         assert (
             set(actual_probabilities.index) ==
             set(expected_probabilities.index)
         )
 
-    # Issue a warning for any column which has high missingness
-    missingness_warning_percent = 100 * missingness_warning_fraction
+
+def warn_if_high_nela_data_missingness(
+    df: pd.DataFrame,
+    warning_fraction: float
+) -> None:
+    """Check that NELA data contain only numeric dtypes. This check should pass
+        with floats, ints and missing values.
+
+    Args:
+        df: NELA data
+        warning_fraction: In interval [0, 1]. Shows warning for any column that
+            has more than this fraction of missing values
+    """
+    missingness_warning_percent = 100 * warning_fraction
     for column_name in df.columns:
         column_percent_missing = percent_missing(df, column_name)
         if column_percent_missing > missingness_warning_percent:
             warn(f'{column_name} has '
                  f'{np.round(column_percent_missing, 1)}% missing values')
-
-    return df
+    for column_name in df.columns:
+        assert is_numeric_dtype(df[column_name])
