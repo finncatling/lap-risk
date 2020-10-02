c7b79a0a7450be63d25ce49f8c7c12a8a892e739
Finn Catling <finn.catling@gmail.com>
2020-10-02 13:40:56 +0100

    lap-risk-24 end-to-end test for preprocess_current()

--- tests/model/test_current.py
+++ tests/model/test_current.py
@@ -1,5 +1,14 @@
 import pandas as pd
 from utils.model import current
+from utils.model.shared import flatten_model_var_dict
+from utils.model.current import (
+    preprocess_current,
+    SplitterTrainerPredictor,
+    WINSOR_THRESHOLDS,
+    CURRENT_MODEL_VARS,
+    CENTRES,
+)
+from utils.split import drop_incomplete_cases
 
 
 def test_discretise_gcs():
@@ -78,9 +87,92 @@ def test_add_asa_age_resp_interaction():
     }).equals(df)
 
 
-def test_preprocess_current():
+def test_preprocess_current(initial_df_permutations_fixture):
     """End-to-end test which just checks that column names are as expected."""
-    assert False
+    df = initial_df_permutations_fixture[
+        flatten_model_var_dict(CURRENT_MODEL_VARS)]
+
+    df, _ = drop_incomplete_cases(df)
+
+    binarize_vars = list(CURRENT_MODEL_VARS["cat"])
+    binarize_vars.remove("S03GlasgowComaScore")
+    binarize_vars.remove("S03ASAScore")
+
+    quadratic_vars = list(CURRENT_MODEL_VARS["cont"])
+    quadratic_vars.remove("S03Sodium")
+    for original, logged in (
+        ("S03SerumCreatinine", "logcreat"),
+        ("S03Urea", "logurea")
+    ):
+        quadratic_vars.remove(original)
+        quadratic_vars.append(logged)
+
+    preprocessed_df, _ = preprocess_current(
+        df,
+        quadratic_vars=quadratic_vars,
+        winsor_thresholds=WINSOR_THRESHOLDS,
+        centres=CENTRES,
+        binarize_vars=binarize_vars,
+        label_binarizers=None,
+    )
+
+    expected_column_names = {
+        "S01Sex",
+        "S03NCEPODUrgency_2",
+        "S03NCEPODUrgency_4",
+        "S03ECG_4",
+        "S03ECG_8",
+        "S03NumberOfOperativeProcedures_4",
+        "S03NumberOfOperativeProcedures_8",
+        "S03CardiacSigns_2",
+        "S03CardiacSigns_4",
+        "S03CardiacSigns_8",
+        "S03Pred_Peritsoil_2",
+        "S03Pred_Peritsoil_4",
+        "S03Pred_Peritsoil_8",
+        "S03Pred_TBL_2",
+        "S03Pred_TBL_4",
+        "S03Pred_TBL_8",
+        "S03DiagnosedMalignancy_2",
+        "S03DiagnosedMalignancy_4",
+        "S03DiagnosedMalignancy_8",
+        "S03WhatIsTheOperativeSeverity",
+        "gcs_3_8",
+        "gcs_9_12",
+        "logcreat",
+        "logcreat_2",
+        "logurea",
+        "logurea_2",
+        "S03Sodium_3",
+        "S03Sodium_3_log",
+        "S03Potassium",
+        "S03Potassium_2",
+        "S03WhiteCellCount",
+        "S03WhiteCellCount_2",
+        "S03Pulse",
+        "S03Pulse_2",
+        "S03SystolicBloodPressure",
+        "S03SystolicBloodPressure_2",
+        "age_asa12",
+        "age_2_asa12",
+        "resp2_asa12",
+        "resp4_asa12",
+        "age_asa3",
+        "age_2_asa3",
+        "resp2_asa3",
+        "resp4_asa3",
+        "age_asa4",
+        "age_2_asa4",
+        "resp2_asa4",
+        "resp4_asa4",
+        "age_asa5",
+        "age_2_asa5",
+        "resp2_asa5",
+        "resp4_asa5",
+        "Target"
+    }
+
+    assert set(preprocessed_df.columns == expected_column_names)
 
 
 class TestSplitterTrainerPredictor:
