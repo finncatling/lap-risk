22667d0ccfe225a254f6e8bb8ffcc6f3535503e2
Finn Catling <finn.catling@gmail.com>
2020-10-08 15:46:56 +0100

    lap-risk-37 Deterministic test for combine_categories()

--- tests/model/test_novel.py
+++ tests/model/test_novel.py
@@ -1,20 +1,32 @@
+import numpy as np
+import pandas as pd
+
 from utils.model import novel
 
 
-def test_combine(initial_df_permutations_fixture):
-    mapping = {"S03ECG": {
-        1: 1,
-        2: 2,
-        4: 4,
-        8: 4,
-    }}
+def test_combine_categories():
     combined_df = novel.combine_categories(
-        initial_df_permutations_fixture,
-        mapping
+        df=pd.DataFrame({
+            'a': [1., 2., 3., np.nan],
+            'b': [4., 5., 4., 6.]
+        }),
+        category_mapping={
+            'a': {
+                1.: 1.,
+                2.: 2.,
+                3.: 2.
+            },
+            'b': {
+                4.: 6.,
+                5.: 5.,
+                6.: 6.
+            },
+        }
     )
-    assert initial_df_permutations_fixture["S03ECG"].unique != combined_df[
-        "S03ECG"
-    ].unique
+    assert all(pd.DataFrame({
+        'a': [1., 2., 2., np.nan],
+        'b': [6., 5., 6., 6.]
+    }) == combined_df)
 
 
 def test_add_missingness_indicators(initial_df_permutations_fixture):
@@ -25,3 +37,7 @@ def test_add_missingness_indicators(initial_df_permutations_fixture):
     )
     cols = df2.shape[1] - cols
     assert cols == 2
+
+
+def test_preprocess_novel_pre_split():
+    assert False
