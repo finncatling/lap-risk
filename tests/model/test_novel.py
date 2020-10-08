e79ddfcab120035ed4465e6cfcca7eb50d47bfaf
Finn Catling <finn.catling@gmail.com>
2020-10-08 17:17:04 +0100

    lap-risk-37 End-to-end test for preprocess_novel_pre_split(). Type declarations

--- tests/model/test_novel.py
+++ tests/model/test_novel.py
@@ -1,8 +1,24 @@
-import pytest
+import copy
 import numpy as np
 import pandas as pd
+import pytest
+from typing import Tuple, Dict
+from pandas.api.types import is_numeric_dtype
 
+from utils.indications import (
+    INDICATION_VAR_NAME,
+    MISSING_IND_CATEGORY,
+    get_indication_variable_names
+)
 from utils.model import novel
+from utils.model.novel import (
+    NOVEL_MODEL_VARS,
+    MULTI_CATEGORY_LEVELS,
+    LACTATE_VAR_NAME,
+    ALBUMIN_VAR_NAME,
+    preprocess_novel_pre_split,
+)
+from utils.model.shared import flatten_model_var_dict
 
 
 @pytest.fixture()
@@ -47,9 +63,70 @@ def test_add_missingness_indicators(missing_categories_df_fixture):
     }).equals(missing_indicator_df)
 
 
-def test_preprocess_novel_pre_split():
-    assert False
-
-
 def test_label_encode():
-    assert False
+    le_df = novel.label_encode(
+        df=pd.DataFrame({
+            'a': [np.nan, 3., 2., 1.],
+            'Indication': ['1', '0', '4', '0']
+        }),
+        multi_cat_levels={
+            'a': (1., 2., 3.),
+            'Indication': ('0', '4')
+        },
+        missing_indication_value='1'
+    )
+    assert pd.DataFrame({
+        'a': [np.nan, 2., 1., 0.],
+        'Indication': [np.nan, 0., 1., 0.]
+    }).equals(le_df)
+
+
+def test_preprocess_novel_pre_split(initial_df_permutations_fixture):
+    """End-to-end test which just checks that dtypes and column names are as
+        expected."""
+    df = initial_df_permutations_fixture
+
+    indications = get_indication_variable_names(df.columns)
+    df = df[flatten_model_var_dict(NOVEL_MODEL_VARS) + indications]
+
+    multi_category_levels: Dict[str, Tuple] = copy.deepcopy(
+        MULTI_CATEGORY_LEVELS)
+    multi_category_levels[INDICATION_VAR_NAME] = tuple(indications)
+
+    df = preprocess_novel_pre_split(
+        df,
+        category_mapping={"S03ECG": {1.0: 0.0, 4.0: 1.0, 8.0: 1.0}},
+        add_missingness_indicators_for=[LACTATE_VAR_NAME, ALBUMIN_VAR_NAME],
+        indication_variable_name=INDICATION_VAR_NAME,
+        indications=indications,
+        missing_indication_value=MISSING_IND_CATEGORY,
+        multi_category_levels=multi_category_levels,
+    )
+
+    for column_name in df.columns:
+        assert is_numeric_dtype(df[column_name])
+
+    assert set(df.columns) == {
+        "S03ASAScore",
+        "S03CardiacSigns",
+        "S03RespiratorySigns",
+        "S03DiagnosedMalignancy",
+        "S03Pred_Peritsoil",
+        "S02PreOpCTPerformed",
+        "S03ECG",
+        "S01AgeOnArrival",
+        "S03SerumCreatinine",
+        "S03PreOpArterialBloodLactate",
+        "S03PreOpLowestAlbumin",
+        "S03PreOpArterialBloodLactate_missing",
+        "S03PreOpLowestAlbumin_missing",
+        "S03Sodium",
+        "S03Potassium",
+        "S03Urea",
+        "S03WhiteCellCount",
+        "S03Pulse",
+        "S03SystolicBloodPressure",
+        "S03GlasgowComaScore",
+        "Target",
+        "Indication"
+    }
