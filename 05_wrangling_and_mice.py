9d8ed73061c2033bcae9aeccd8a332a7fbe4cd92
Finn Catling <finn.catling@gmail.com>
2020-10-09 14:06:22 +0100

    lap-risk-44 debug

--- 05_wrangling_and_mice.py
+++ 05_wrangling_and_mice.py
@@ -81,7 +81,7 @@ df.to_pickle(os.path.join(DATA_DIR, "05_preprocessed_df.pkl"))
 
 
 reporter.report("Determining fraction of cases with missing data")
-n_imputations, fraction_incomplete_cases = determine_n_imputations()
+n_imputations, fraction_incomplete_cases = determine_n_imputations(df)
 print(f"{np.round(100 * fraction_incomplete_cases, 2)}% of cases have missing "
       f"data, so we will perform {n_imputations} imputations")
 
