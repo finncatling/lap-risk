c0ca0c6f77f421359653bb00c45a002732cef864
Finn Catling <finn.catling@gmail.com>
2020-10-11 08:59:00 +0100

    reduced splits for testing purposes

--- 05_wrangling_and_mice.py
+++ 05_wrangling_and_mice.py
@@ -138,6 +138,10 @@ mice_cont_vars.remove(LACTATE_VAR_NAME)
 mice_cont_vars.remove(ALBUMIN_VAR_NAME)
 
 
+# TODO: Remove this testing line
+tt_splitter.n_splits = 1
+
+
 reporter.report("Running MICE")
 swm = SplitterWinsorMICE(
     df=mice_df,
