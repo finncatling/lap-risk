5b4613ac17f00bcfd5c9bbc42c6913cebb789859
Finn Catling <finn.catling@gmail.com>
2020-10-15 19:19:40 +0100

    lap-risk-52 Changed bash script to run categorical imputation too

--- run_full_analysis_on_bdau.sh
+++ run_full_analysis_on_bdau.sh
@@ -8,7 +8,7 @@ pipenv run taskset -c 32-39 python 02_train_eval_current_model.py
 pipenv run taskset -c 32-39 python 03_plot_current_model.py
 pipenv run taskset -c 32-39 python 04_consolidate_indications.py
 pipenv run taskset -c 32-39 python 05_wrangling_and_mice.py
-#pipenv run taskset -c 32-39 python 06_categorical_imputation.py
+pipenv run taskset -c 32-39 python 06_categorical_imputation.py
 #pipenv run taskset -c 32-39 python 07_albumin_imputation.py
 #pipenv run taskset -c 32-39 python 08_lactate_imputation.py
 #pipenv run taskset -c 32-39 python 09_train_eval_novel_model.py
