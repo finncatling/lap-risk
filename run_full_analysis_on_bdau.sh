5bf6e68e24d888d96ff8b45cc3ab2d48231daae2
Finn Catling <finn.catling@gmail.com>
2020-11-09 14:56:52 +0000

    added model comparison script

--- run_full_analysis_on_bdau.sh
+++ run_full_analysis_on_bdau.sh
@@ -12,3 +12,4 @@ pipenv run taskset -c 32-39 python 06_categorical_imputation.py
 pipenv run taskset -c 32-39 python 07_albumin_lactate_imputation.py
 pipenv run taskset -c 32-39 python 08_train_eval_novel_model.py
 pipenv run taskset -c 32-39 python 09_plot_novel_model.py
+pipenv run taskset -c 32-39 python 10_compare_models.py
