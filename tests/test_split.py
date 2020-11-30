072eda85fdee91f4dfe7e578968c831fc76e14cc
finncatling <finn.catling@gmail.com>
2020-11-30 10:32:47 +0000

    lap-risk-98 typo

--- tests/test_split.py
+++ tests/test_split.py
@@ -117,7 +117,7 @@ def test_tt_splitter_all_test_case_modifier(train_test_split_fixture):
     mod_tts = split.tt_splitter_all_test_case_modifier(original_tts)
     for split_i in range(original_tts.n_splits):
         assert all(original_tts.train_i[split_i] == mod_tts.train_i[split_i])
-        # TODO: modify fixture to for meaningful test of test fold inequality
+        # TODO: modify fixture for meaningful test of test fold inequality
 
 
 @pytest.fixture(scope='function')
