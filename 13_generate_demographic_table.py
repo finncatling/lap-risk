2188471da80458727adf9aa5d960a37fa20d1ea2
finncatling <finn.catling@gmail.com>
2020-11-30 17:19:13 +0000

    lap-risk-98 Draft Table 1 function for continuous variables

--- 13_generate_demographic_table.py
+++ 13_generate_demographic_table.py
@@ -4,7 +4,7 @@ import pandas as pd
 from utils.constants import DATA_DIR, TABLES_OUTPUT_DIR, INTERNAL_OUTPUT_DIR
 from utils.io import load_object
 from utils.split import TrainTestSplitter, tt_splitter_all_test_case_modifier
-from utils.table import Table1Variable
+from utils.table import DemographicTableVariable, generate_demographic_table
 from utils.report import Reporter
 
 
@@ -12,16 +12,20 @@ reporter = Reporter()
 reporter.title("Generate Table 1 (demographic information")
 
 
-reporter.report('Loading raw data')
+reporter.report('Loading preprocessed data')
+# These data are not Winsorized, so safe to use for summary stats
+df = pd.read_pickle(os.path.join(DATA_DIR, "05_preprocessed_df.pkl"))
+
+
+reporter.report(
+    'Loading raw data and adding absent variables to preprocessed data'
+)
 raw_df = pd.read_pickle(os.path.join(
     DATA_DIR,
     "lap_risk_df_after_univariate_wrangling_all_variables.pkl"
 ))
-
-
-reporter.report('Loading preprocessed data')
-# These data are not Winsorized, so safe to use for summary stats
-df = pd.read_pickle(os.path.join(DATA_DIR, "05_preprocessed_df.pkl"))
+for variable_name in ("S01Sex",):  # TODO: Add more variables here if needed
+    df[variable_name] = raw_df[variable_name]
 
 
 reporter.report("Loading data needed for train-test splitting")
@@ -34,50 +38,104 @@ reporter.report("Modifying train-test splitter to include all test cases")
 tt_splitter = tt_splitter_all_test_case_modifier(tt_splitter)
 
 
-reporter.report('Defining Table 1 specification')
-table_1_spec = (
-    Table1Variable(
+reporter.report('Specifying Table 1 variables')
+table_1_variables = (
+    DemographicTableVariable(
         "S01AgeOnArrival",
         "Age (years)",
         True,
         'continuous'
     ),
-    Table1Variable(
-        "S01Sex",
-        "Female",
-        False,
-        'binary',
-        True
-    ),
-    Table1Variable(
-        "Target",
-        "Died",
+    DemographicTableVariable(
+        "S03Pulse",
+        "Heart rate (BPM)",
         True,
-        'binary'
+        'continuous'
     ),
-    Table1Variable(
-        "S03ASAScore",
-        "ASA physical status",
-        True,
-        'ordinal_multicat'
-    ),
-    Table1Variable(
-        "S03Pulse",),
-    Table1Variable(
+    DemographicTableVariable(
         "S03SystolicBloodPressure",
-        "Systolic pressure (mmHg)"),
-    Table1Variable(
-        "S03CardiacSigns",
-        "Cardiovascular status",
+        "Systolic pressure (mmHg)",
         True,
-        'ordinal_multicat'
+        'continuous'
     ),
-    Table1Variable(
-        "S03RespiratorySigns",
-        "Respiratory status",
+    DemographicTableVariable(
+        "S03Sodium",
+        "Sodium (mmol/L)",
         True,
-        'ordinal_multicat'
+        'continuous'
     ),
+    DemographicTableVariable(
+        "S03Potassium",
+        "Potassium (mmol/L)",
+        True,
+        'continuous'
+    ),
+    DemographicTableVariable(
+        "S03WhiteCellCount",
+        r"White cell count ($\times$10${^9}$/L)",
+        True,
+        'continuous'
+    ),
+    DemographicTableVariable(
+        "S03SerumCreatinine",
+        "Creatinine (mmol/L)",
+        True,
+        'continuous'
+    ),
+    DemographicTableVariable(
+        "S03Urea",
+        "Urea (mmol/L)",
+        True,
+        'continuous'
+    ),
+    DemographicTableVariable(
+        "S03PreOpArterialBloodLactate",
+        "Lactate (mmol/L)",
+        True,
+        'continuous'
+    ),
+    DemographicTableVariable(
+        "S03PreOpLowestAlbumin",
+        "Albumin (g/L)",
+        True,
+        'continuous'
+    ),
+    DemographicTableVariable(
+        "S03GlasgowComaScore",
+        "Glasgow Coma Score",
+        True,
+        "continuous"  # this approximation suffices here
+    ),
+    # Table1Variable(
+    #     "S01Sex",
+    #     "Female",
+    #     False,
+    #     'binary',
+    # ),
+    # Table1Variable(
+    #     "Target",
+    #     "Died",
+    #     True,
+    #     'binary'
+    # ),
+    # Table1Variable(
+    #     "S03ASAScore",
+    #     "ASA physical status",
+    #     True,
+    #     'ordinal_multicat'
+    # ),
+    # Table1Variable(
+    #     "S03CardiacSigns",
+    #     "Cardiovascular status",
+    #     True,
+    #     'ordinal_multicat'
+    # ),
+    # Table1Variable(
+    #     "S03RespiratorySigns",
+    #     "Respiratory status",
+    #     True,
+    #     'ordinal_multicat'
+    # ),
     # Table1Variable(
     #     "S03ECG",  # TODO: Need to use binarized version
     #     "Non-sinus rhythm",
@@ -99,38 +157,23 @@ table_1_spec = (
     # Table1Variable(
     #     "S03WhatIsTheOperativeSeverity",),
     # Table1Variable(
-    #     "S03GlasgowComaScore",
-    #     "Glasgow Coma Score",
-    #     True,
-    #     "ordinal_multicat"
-    # ),
-    # Table1Variable(
-    #     "S03SerumCreatinine",
-    #     "Creatinine (mmol/L)",
-    #     True,
-    #     'continuous'
-    # ),
-    # Table1Variable(
-    #     "S03Sodium",),
-    # Table1Variable(
-    #     "S03Potassium",),
-    # Table1Variable(
-    #     "S03Urea",),
-    # Table1Variable(
-    #     "S03WhiteCellCount",
-    #     r"White cell count ($\times$10${^9}$/L)"),
-    # Table1Variable(
     #     "S02PreOpCTPerformed",),
-    # Table1Variable(
-    #     "S03PreOpArterialBloodLactate",),
-    # Table1Variable(
-    #     "S03PreOpLowestAlbumin",),
     # Table1Variable(
     #     "S03Pred_TBL",),
     # Table1Variable(
     #     "S03NumberOfOperativeProcedures",),
     # Table1Variable(
     #     "S03NCEPODUrgency",),
+)
+
+
+print(
+    generate_demographic_table(
+        variables=table_1_variables,
+        df=df,
+        modified_tts=tt_splitter,
+        output_filepath='FILL THIS IN PROPERLY'
+    )
 )
 
 
--- utils/table.py
+++ utils/table.py
@@ -1,30 +1,58 @@
 from typing import Tuple
+from collections import OrderedDict
 from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
+from utils.split import TrainTestSplitter
+
 
 @dataclass
-class Table1Variable:
+class DemographicTableVariable:
     name: str
     pretty_name: str
     in_novel_model: bool
     var_type: str  # {'continuous', 'binary', 'ordinal_multicat', 'multicat'}
-    raw_data: bool = False  # If True, get from raw data, not preprocessed
     decimal_places: int = 0
 
 
 def generate_demographic_table(
-    specification: Tuple[Table1Variable, ...],
-    preprocessed_df: pd.DataFrame,
-    raw_df: pd.DataFrame,
+    variables: Tuple[DemographicTableVariable, ...],
+    df: pd.DataFrame,
+    modified_tts: TrainTestSplitter,
     output_filepath: str
 ):
+    dfs = OrderedDict()
+    dfs['all'] = df
+    dfs['train'] = df.loc[modified_tts.train_i].copy().reset_index(drop=True)
+    dfs['test'] = df.loc[modified_tts.test_i].copy().reset_index(drop=True)
+
     table = pd.DataFrame(
-        data=np.zeros((len(specification), 6)),
+        data=np.zeros((len(variables), 6)),
         columns=(
             'Variable',
-            f'All cases (n={preprocessed_df.shape[0]})'
+            f'All cases (n={dfs["all"].shape[0]})',
+            'Missing values (%)',
+            f'Development cases (n={dfs["train"].shape[0]})',
+            f'Evalulation cases (n={dfs["test"].shape[0]})',
+            'In novel model'
         ),
         dtype=str
     )
+
+    for var_i, var in enumerate(variables):
+        if var.var_type == 'continuous':
+            table.loc[var_i, 'Variable'] = f'{var.pretty_name}: median (IQR)'
+            for df_i, df in enumerate(dfs.values()):
+                quantiles = df[var.name].quantile([0.25, 0.5, 0.75])
+                quantiles = np.round(quantiles, var.decimal_places).astype(str)
+                table.iloc[var_i, df_i + 1] = (
+                    f'{quantiles[1]} ({quantiles[0]} - {quantiles[1]})')
+        elif var.var_type == 'binary':
+            pass
+        elif var.var_type == 'ordinal_multicat':
+            pass
+        elif var.var_type == 'multicat':
+            pass
+
+    return table
